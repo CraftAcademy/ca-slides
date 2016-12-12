fb335499c9489dcd6fe4e4d0b64f2e3c660eaac7
Raoul DIFFOUO <diraulo@gmail.com>
2016-12-12 16:34:45 +0200

    setup deployment with middleman-deploy using rsync

--- config.rb
+++ config.rb
@@ -1,10 +1,5 @@
-# Dir['extensions/*.rb'].each { |file| require file }
-require 'extensions/remarky'
+Dir['extensions/*.rb'].each { |file| require file }
 activate :remarky
-
-###
-# Page options, layouts, aliases and proxies
-###
 
 # Per-page layout changes:
 #
@@ -12,13 +7,6 @@ activate :remarky
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
-
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
 
 remarks.each do |r|
   remark_name = File.basename(r)
@@ -39,10 +27,17 @@ end
 
 # Build-specific configuration
 configure :build do
-  # Minify CSS on build
-  # activate :minify_css
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
 
-  # Minify Javascript on build
-  # activate :minify_javascript
   ignore 'remark.html.haml'
+end
+
+activate :deploy do |deploy|
+  deploy.deploy_method   = :rsync
+  deploy.host            = ENV.fetch('DEPLOY_HOST')
+  deploy.path            = ENV.fetch('DEPLOY_DIR')
+  deploy.build_before    = true
+  deploy.clean           = true
 end
